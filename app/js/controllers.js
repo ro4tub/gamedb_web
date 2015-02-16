1f4ebc437a576a2c59160dac5572a4c0bb9dd272
huangkaifeng <ro4tub@gmail.com>
2015-02-16 22:50:50 +0800

    动态修改html title

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,20 +4,22 @@
 
 var gamedbControllers = angular.module('gamedbControllers', []);
 
-gamedbControllers.controller('GameSearchCtrl', ['$scope', 'GameSearch',
-  function($scope, GameSearch) {
+gamedbControllers.controller('GameSearchCtrl', ['$scope', '$rootScope', 'GameSearch',
+  function($scope, $rootScope, GameSearch) {
 	  $scope.games = [];
 	  $scope.submit = function () {
 		  if($scope.text) {
 		  	 // rest查询
 		     $scope.games = GameSearch.query({keyword: $scope.text});
 		  }
-	  }	
+	  }
+	  $rootScope.pageTitle = "Welcome"
   }]);
 
-gamedbControllers.controller('GameDetailCtrl', ['$scope', '$routeParams', 'Game',
-  function($scope, $routeParams, Game) {
+gamedbControllers.controller('GameDetailCtrl', ['$scope', '$rootScope', '$routeParams', 'Game',
+  function($scope, $rootScope, $routeParams, Game) {
     $scope.game = Game.get({gameId: $routeParams.gameId}, function(game) {
 	  	$scope.markdown = game.DetailDesc
+		$rootScope.pageTitle = game.Name
     });
   }]);
