88b19aa9198f6a3f5b93c68a7f474e1a699b4d82
Ying Sha <babafish@gmail.com>
2015-03-18 03:15:54 +0800

    fix service.js, change localhost to domain name

--- app/js/services.js
+++ app/js/services.js
@@ -6,19 +6,23 @@ var gamedbServices = angular.module('gamedbServices', ['ngResource']);
 
 gamedbServices.factory('Game', ['$resource',
   function($resource){
-    return $resource('http://localhost:8080/gamedbapi/game/:gameId', {}, {
+    //return $resource('http://localhost:8080/gamedbapi/game/:gameId', {}, {
+    var res= $resource('http://huangyaoshi.me:8080/gamedbapi/game/:gameId', {}, {
       // query: {method:'GET', params:{gameId:'games'}, isArray:true}
     });
+    return res;
   }]);
 
 gamedbServices.factory('GameSearch', ['$resource',
   function($resource){
-    return $resource('http://localhost:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {
-    });
+    //return $resource('http://localhost:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {
+    var res = $resource('http://huangyaoshi.me:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {});
+    return res;
   }]);
 
 gamedbServices.factory('Homepage', ['$resource',
   function($resource){
-    return $resource('http://localhost:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {
-    });
+    //return $resource('http://localhost:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {
+    var res = $resource('http://huangyaoshi.me:8080/gamedbapi/search?v=:keyword&p=:platform&g=:genre', {}, {});
+    return res;
   }]);
